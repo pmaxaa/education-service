702d1eabd651cd04919023a2037ea9dfddf31e99
Eblaraa <baraax01@gmail.com>
2024-05-18 06:30:16 +0700

    i added some editing in style of checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.scss";
-import { MyVideo } from "./Shared";
+import { Checkbox, MyVideo } from "./Shared";
 
 function App() {
   return (
     <div className="app">
       {/* <MyVideo LinkVideo='https://www.youtube.com/watch?v=oWxL4UQvH-U'/> */}
-      <MyVideo LinkVideo='https://vk.com/video_ext.php?oid=-220754053&id=456240680&hd=2'/>
+      {/* <MyVideo LinkVideo='https://vk.com/video_ext.php?oid=-220754053&id=456240680&hd=2'/> */}
       {/* <MyVideo LinkVideo='https://vk.com/video_ext.php?oid=-219407444&id=456239041&hd=2'/> */}
       {/* <MyVideo LinkVideo='https://rutube.ru/video/f661e71761b22ff66aaa1255c567337e/?r=wd'/> */}
+      <Checkbox/>
     </div>
   );
 }
