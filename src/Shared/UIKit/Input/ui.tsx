1bf998dd047c6742bb93b53a9fdbe9e327b542c5
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2024-05-18 17:37:58 +0700

    Add dark theme for Input component

--- src/Shared/UIKit/Input/ui.tsx
+++ src/Shared/UIKit/Input/ui.tsx
@@ -3,17 +3,22 @@ import "./style.scss";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  theme?: "light" | "dark";
 }
 
 const InputField = forwardRef(
-  ({ label, className, ...props }: InputProps, ref) => {
+  ({ theme = "light", label, className, ...props }: InputProps, ref) => {
     const innerRef = useRef<HTMLInputElement>(null);
     useImperativeHandle(ref, () => innerRef.current);
 
     return (
       <label className={"input" + (className ? " " + className : "")}>
         {label && <span className="input__title">{label}</span>}
-        <input {...props} className="input__field" ref={innerRef} />
+        <input
+          {...props}
+          className={"input__field input__field_theme_" + theme}
+          ref={innerRef}
+        />
       </label>
     );
   }
