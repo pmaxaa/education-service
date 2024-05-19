56e1ee0d0091ddef838b60872f30853f9168457d
pmaxaa <penckowamaria198@gmail.com>
2024-05-20 03:34:21 +0700

    Update store.ts
    add state for teachers

--- src/Shared/lib/store/store.ts
+++ src/Shared/lib/store/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { Course, Lesson } from '../types/types';
+import { Course, Lesson, Teacher } from '../types/types';
 
 //к вечеру будут функции для изменения статусов уроков и курсов ()
 type CourseState = {
@@ -12,51 +12,66 @@ type LessonState = {
 	setLessons: (lessons: Lesson[]) => void;
 };
 
+type TeacherState = {
+	teachers: Teacher[];
+	setTeachers: (teachers: Teacher[]) => void;
+};
+
 type ThemeState = {
 	theme: 'light' | 'dark';
 	changeTheme: () => void;
 };
 
-const useGlobalStore = create<CourseState & LessonState & ThemeState>()(
-	set => ({
-		courses: [],
-		lessons: [],
-		theme: 'light',
+const useGlobalStore = create<
+	CourseState & LessonState & TeacherState & ThemeState
+>()(set => ({
+	courses: [],
+	lessons: [],
+	teachers: [],
+	theme: 'light',
 
-		setCourses: courses => {
-			set(state => {
+	setCourses: courses => {
+		set(state => {
+			return {
+				...state,
+				courses,
+			};
+		});
+	},
+
+	setLessons: lessons => {
+		set(state => {
+			return {
+				...state,
+				lessons,
+			};
+		});
+	},
+
+	setTeachers: teachers => {
+		set(state => {
+			return {
+				...state,
+				teachers,
+			};
+		});
+	},
+
+	changeTheme: () => {
+		set(state => {
+			if (state.theme === 'dark') {
 				return {
 					...state,
-					courses,
+					theme: 'light',
 				};
-			});
-		},
-
-		setLessons: lessons => {
-			set(state => {
+			} else {
 				return {
 					...state,
-					lessons,
+					theme: 'dark',
 				};
-			});
-		},
-
-		changeTheme: () => {
-			set(state => {
-				if (state.theme === 'dark') {
-					return {
-						...state,
-						theme: 'light',
-					};
-				} else {
-					return {
-						...state,
-						theme: 'dark',
-					};
-				}
-			});
-		},
-	})
-);
+			}
+		});
+	},
+}));
 
 export default useGlobalStore;
