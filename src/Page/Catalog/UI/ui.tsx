b4529b789807f994dd8b3287a947d401c1cc6a0b
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2024-05-20 04:17:11 +0700

    Created CoursesList component for catalog page

--- src/Page/Catalog/UI/ui.tsx
+++ src/Page/Catalog/UI/ui.tsx
@@ -4,12 +4,14 @@ import { useLocation } from "react-router-dom";
 import { MainHeader } from "../../../Widgets/MainHeader/ui";
 import { CategoriesList } from "../Components/CategoriesList/ui";
 import { Filter, filterStateType } from "../Components/Filter/ui";
+import { CoursesList } from "../Components/CoursesList/ui";
 
 export const CatalogPage = () => {
   const location = useLocation();
   console.log("CatalogPage state", location.state);
 
   const [CurrentCategory, setCurrentCategory] = useState<string>("");
+  const [TextFilter, setTextFilter] = useState<string>("");
   const [Theme, setTheme] = useState<"light" | "dark">("light");
 
   const [FilterState, setFilterState] = useState<filterStateType>({
@@ -22,8 +24,13 @@ export const CatalogPage = () => {
 
   return (
     <>
-      <MainHeader theme={Theme} setTheme={setTheme} />
-      <main className="catalogPage__main">
+      <MainHeader
+        theme={Theme}
+        setTheme={setTheme}
+        textFilter={TextFilter}
+        setTextFilter={setTextFilter}
+      />
+      <main className={"catalogPage__main catalogPage__main_theme_" + Theme}>
         <div className="catalogPage__mainWrap">
           <CategoriesList
             currentCategory={CurrentCategory}
@@ -34,6 +41,12 @@ export const CatalogPage = () => {
             theme={Theme}
             filterState={FilterState}
             setFilterState={setFilterState}
+          />
+          <CoursesList
+            currentCategory={CurrentCategory}
+            textFilter={TextFilter}
+            filterState={FilterState}
+            theme={Theme}
           />
         </div>
       </main>
