876bcfa3d109f8a4830dcaaebb7db7adca7b9364
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2024-05-19 14:24:20 +0700

    Add dark theme in the main page

--- src/Page/Main/UI/ui.tsx
+++ src/Page/Main/UI/ui.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { MainHeader } from "../../../Widgets/MainHeader/ui";
 import { InfoList } from "../Component/InfoList/ui";
+import { CoursesList } from "../Component/CoursesList/ui";
+import { TeachersList } from "../Component/TeachersList/ui";
 
 export const MainPage = () => {
+  const [Theme, setTheme] = useState<"light" | "dark">("light");
   return (
     <>
-      <MainHeader />
-      <main className="mainPage">
-        <InfoList />
+      <MainHeader theme={Theme} setTheme={setTheme} />
+      <main className={"mainPage" + " mainPage_theme_" + Theme}>
+        <InfoList theme={Theme} />
+        <CoursesList theme={Theme} />
+        <TeachersList theme={Theme} />
       </main>
-      ;
     </>
   );
 };
