f01b452595968c05aa364a5474244e68a87426cf
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2024-05-20 03:09:47 +0700

    Created Filter component for catalog

--- src/Page/Catalog/UI/ui.tsx
+++ src/Page/Catalog/UI/ui.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.scss";
 import { useLocation } from "react-router-dom";
 import { MainHeader } from "../../../Widgets/MainHeader/ui";
+import { CategoriesList } from "../Components/CategoriesList/ui";
+import { Filter, filterStateType } from "../Components/Filter/ui";
 
 export const CatalogPage = () => {
   const location = useLocation();
   console.log("CatalogPage state", location.state);
+
+  const [CurrentCategory, setCurrentCategory] = useState<string>("");
+  const [Theme, setTheme] = useState<"light" | "dark">("light");
+
+  const [FilterState, setFilterState] = useState<filterStateType>({
+    cost: [],
+    level: [],
+    duration: [],
+    format: [],
+    status: [],
+  });
+
   return (
     <>
-      <MainHeader />
-      <main>CatalogPage</main>;
+      <MainHeader theme={Theme} setTheme={setTheme} />
+      <main className="catalogPage__main">
+        <div className="catalogPage__mainWrap">
+          <CategoriesList
+            currentCategory={CurrentCategory}
+            setCurrentCategory={setCurrentCategory}
+            theme={Theme}
+          />
+          <Filter
+            theme={Theme}
+            filterState={FilterState}
+            setFilterState={setFilterState}
+          />
+        </div>
+      </main>
     </>
   );
 };
